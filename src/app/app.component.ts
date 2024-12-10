0fde606c525a04f5bc5e056d7634c71ac326767e
antodrade <antoandrade1988@gmail.com>
2024-12-09 21:33:57 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,11 +37,88 @@ export class AppComponent {
     // Llamar al servicio para agregar texto en las coordenadas especificadas
     const text = 'Andrade';
     const text2 = 'Villa';
+    const text3 = 'Antonio';
+    const text4 = 'Maria';
+    const text5 = 'CC';
+    const text6 = '1065594047';
+    const text7 = 'x'
+    const text8 = '0  7 0 6 1 9 8  8'
+    const text9 = 'ARL SURA';
+    const text10 = 'Colfondos';
+    const text11 = '1300000'
+    const text12 = 'Calle 57 # 43-43'
+    const text13 = '3452012';
+    const text14 = '3005036475';
+    const text15 = 'antoandrade1988@gmail.com';
+    const text16 = 'Barranquilla';
+    const text17 = 'x';
+    const text18 = '';
+    const text19 = 'Atlántico'
+    const text20 = '0  7 0 6 1 9 8  8'
+    const text21 = 'CC';
+    const text22 = '1065594047';
+    const text23 = 'x'
+    const text24 = '0  7 0 6 1 9 8  8'
+    const text25 = '1065594047';
+    const text26 = 'x'
+    const text27 = '0  7 0 6 1 9 8  8'
     const x = 35;  // Coordenada X
     const y = 628;  // Coordenada Y
-    const x2 = 95;  // Coordenada X
+    const x2 = 170;  // Coordenada X
     const y2 = 628;  // Coordenada Y
-    this.pdfService.addTextToPdf(existingPdfBytes, text,text2, x, y, x2, y2);
+    const x3 = 308;  // Coordenada X
+    const y3 = 628;  // Coordenada Y
+    const x4 = 446;  // Coordenada X
+    const y4 = 628;  // Coordenada Y
+    const x5 = 35;  // Coordenada X
+    const y5 = 600;  // Coordenada Y
+    const x6 = 170;  // Coordenada X
+    const y6 = 600;  // Coordenada Y
+    const x7 = 385;  // Coordenada X
+    const y7 = 603;  // Coordenada Y
+    const x8 = 492;  // Coordenada X
+    const y8 = 603;  // Coordenada Y
+    const x9 = 35;  // Coordenada X     ok
+    const y9 = 540;  // Coordenada Y    ok
+    const x10 = 220;  // Coordenada     
+    const y10 = 540;  // Coordenada Y
+    const x11 = 410;  // Coordenada X
+    const y11 = 540;  // Coordenada Y
+    const x12 = 35;  // Coordenada X
+    const y12 = 522;  // Coordenada Y
+    const x13 = 250;  // Coordenada X
+    const y13 = 522;  // Coordenada Y
+    const x14 = 370;  // Coordenada X
+    const y14 = 522;  // Coordenada Y
+    const x15 = 475;  // Coordenada X
+    const y15 = 522;  // Coordenada Y
+    const x16 = 35;  // Coordenada X
+    const y16 = 504;  // Coordenada Y
+    const x17 = 218;  // Coordenada X
+    const y17 = 504;  // Coordenada Y
+    const x18 = 308;  // Coordenada X
+    const y18 = 504;  // Coordenada Y
+    const x19 = 446;  // Coordenada X
+    const y19 = 504;  // Coordenada Y
+    const x20 = 446;  // Coordenada X
+    const y20 = 628;  // Coordenada Y
+    const x21 = 35;  // Coordenada X
+    const y21 = 600;  // Coordenada Y
+    const x22 = 170;  // Coordenada X
+    const y22 = 600;  // Coordenada Y
+    const x23 = 385;  // Coordenada X
+    const y23 = 603;  // Coordenada Y
+    const x24 = 492;  // Coordenada X
+    const y24 = 603;  // Coordenada Y
+    const x25 = 35;  // Coordenada X
+    const y25 = 628;  // Coordenada Y
+    const x26 = 170;  // Coordenada X
+    const y26 = 628;  // Coordenada Y
+    const x27 = 308;  // Coordenada X
+    const y27 = 628;  // Coordenada Y
+    this.pdfService.addTextToPdf(existingPdfBytes, text,text2,text3,text4,text5,text6,text7,text8,
+      text9,text10,text11,text12,text13,text14,text15,text16,text17,text18,text19,x, y, x2, y2, x3, y3, x4, y4,x5,y5, x6, y6, x7, y7, x8, y8,
+      x9, y9, x10, y10, x11, y11, x12, y12,x13,y13, x14, y14, x15, y15, x16,y16, x17, y17, x18, y18 , x19, y19);
   }
 }
 
--- src/app/pdf.service.ts
+++ src/app/pdf.service.ts
@@ -20,8 +20,11 @@ export class PdfService {
 
   constructor() { }
 
-  async addTextToPdf(existingPdfBytes: Uint8Array, text: string, text2: string, x: number, y: number,
-     x2: number, y2: number) {
+  async addTextToPdf(existingPdfBytes: Uint8Array, text: string, text2: string, text3: string, text4: string, text5: string, text6: string, text7: string,text8: string,
+    text9: string, text10: string, text11: string, text12: string, text13: string, text14: string, text15: string, text16: string, text17: string, text18: string, text19: string,
+    x: number, y: number, x2: number, y2: number,  x3: number, y3: number, x4: number, y4: number, x5: number, y5: number,x6: number, y6: number,
+     x7: number, y7: number,x8: number, y8: number, x9: number, y9: number, x10: number, y10: number,  x11: number, y11: number, x12: number, y12: number, 
+     x13: number, y13: number,x14: number, y14: number,x15: number, y15: number,  x16: number, y16: number,x17: number, y17: number,x18: number, y18: number ,x19: number, y19: number) {
     // Cargar el PDF existente
     const pdfDoc = await PDFDocument.load(existingPdfBytes);
 
@@ -48,6 +51,149 @@ export class PdfService {
       color: rgb(0, 0, 0),  // Color negro
     });
 
+    page.drawText(text3, {
+      x: x3,
+      y: y3,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text4, {
+      x: x4,
+      y: y4,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text5, {
+      x: x5,
+      y: y5,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text6, {
+      x: x6,
+      y: y6,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text7, {
+      x: x7,
+      y: y7,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text8, {
+      x: x8,
+      y: y8,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text8, {
+      x: x8,
+      y: y8,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text9, {
+      x: x9,
+      y: y9,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text10, {
+      x: x10,
+      y: y10,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text11, {
+      x: x11,
+      y: y11,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text12, {
+      x: x12,
+      y: y12,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text13, {
+      x: x13,
+      y: y13,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text14, {
+      x: x14,
+      y: y14,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text15, {
+      x: x15,
+      y: y15,
+      font: font,
+      size: 8,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text16, {
+      x: x16,
+      y: y16,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text17, {
+      x: x17,
+      y: y17,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text18, {
+      x: x18,
+      y: y18,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
+
+    page.drawText(text19, {
+      x: x19,
+      y: y19,
+      font: font,
+      size: 12,
+      color: rgb(0, 0, 0),  // Color negro
+    });
     // Guardar el PDF modificado
     const pdfBytes = await pdfDoc.save();
 
